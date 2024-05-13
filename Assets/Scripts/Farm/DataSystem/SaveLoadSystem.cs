77628fd67f3253abd149736ad670798e45a8b346
MorasDeFresa <david@castromora.lat>
2024-05-13 08:41:19 -0500

    refactor(SaveLoadSystem): include harvest minigame and level1

--- Assets/Scripts/Farm/DataSystem/SaveLoadSystem.cs
+++ Assets/Scripts/Farm/DataSystem/SaveLoadSystem.cs
@@ -12,6 +12,7 @@ using UnityEngine.SceneManagement;
     public PlayerData PlayerData;
     public List<PotData> PotDatas;
     public List<LandData> LandDatas;
+    public List<BasketData> BasketDatas;
 }
 
 public interface ISaveable
@@ -31,6 +32,7 @@ public class SaveLoadSystem : PersistentSingleton<SaveLoadSystem>
    [SerializeField] public GameData gameData;
    public Crops crops;
    public FarmerTerrain farmerTerrain;
+   public HarvestBaskets harvestBaskets;
 
     IDataService dataService;
 
@@ -49,7 +51,8 @@ public class SaveLoadSystem : PersistentSingleton<SaveLoadSystem>
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         if(scene.name == "Menu") return;
-        LoadListData();
+        if(scene.name == "Level1")LoadListData();
+        Bind<Basket,BasketData>(gameData.BasketDatas);
         Bind<Farmer,PlayerData>(gameData.PlayerData);
         Bind<Pot,PotData>(gameData.PotDatas);
         Bind<Land,LandData>(gameData.LandDatas);
@@ -57,11 +60,28 @@ public class SaveLoadSystem : PersistentSingleton<SaveLoadSystem>
 
     void LoadListData()
     {
-        if(gameData.PotDatas.Count>0)crops.loadPot(gameData.PotDatas);  
-        else crops.newPot(new UnityEngine.Vector3(0,0,0),5);  
+        if(gameData.PotDatas.Count>0) crops.loadPot(gameData.PotDatas);
+        else
+        {
+            crops.newPot(new UnityEngine.Vector3(15f,-0.7f,47.5f),5);
+            crops.newPot(new UnityEngine.Vector3(15f,-0.7f,45.5f),5);
+            crops.newPot(new UnityEngine.Vector3(15f,-0.7f,43.5f),5);
+        }  
 
         if(gameData.LandDatas.Count>0)farmerTerrain.loadLand(gameData.LandDatas);  
-        else farmerTerrain.newLand(new UnityEngine.Vector3(4,0,4),5);  
+        else
+        {
+            farmerTerrain.newLand(new UnityEngine.Vector3(-10.63377f,-2.65f,47f),11); 
+            farmerTerrain.newLand(new UnityEngine.Vector3(-13.5f,-2.65f,47f),11); 
+            farmerTerrain.newLand(new UnityEngine.Vector3(-16f,-2.65f,47f),11); 
+            farmerTerrain.newLand(new UnityEngine.Vector3(-18.5f,-2.65f,47f),11); 
+            // farmerTerrain.newLand(new UnityEngine.Vector3(-23f,-2.6f,47f),11); 
+            // farmerTerrain.newLand(new UnityEngine.Vector3(-21f,-2.6f,47f),11);
+            // farmerTerrain.newLand(new UnityEngine.Vector3(-19f,-2.6f,47f),11);
+        }
+
+        if(gameData.BasketDatas.Count>0)harvestBaskets.loadBasket(gameData.BasketDatas);  
+        else harvestBaskets.newBasket(new UnityEngine.Vector3(-40.84656f,0,110.3496f),5); 
     }
 
 
@@ -94,7 +114,7 @@ public class SaveLoadSystem : PersistentSingleton<SaveLoadSystem>
     {
         gameData = new GameData{
             Name = "New Game",
-            CurrentLevelName = "Demo",
+            CurrentLevelName = "Level1",
             PlayerData = new PlayerData
             {
                 PlayerPosition = new UnityEngine.Vector3(0,0,0),
@@ -103,7 +123,8 @@ public class SaveLoadSystem : PersistentSingleton<SaveLoadSystem>
                 HandItem = SerializableGuid.Empty
             },
             PotDatas = new List<PotData>(),
-            LandDatas = new List<LandData>()
+            LandDatas = new List<LandData>(),
+            BasketDatas = new List<BasketData>()
 
         };
         SceneManager.LoadScene(gameData.CurrentLevelName);
@@ -120,7 +141,7 @@ public class SaveLoadSystem : PersistentSingleton<SaveLoadSystem>
 
         if (String.IsNullOrWhiteSpace(gameData.CurrentLevelName))
         {
-            gameData.CurrentLevelName = "Demo";
+            gameData.CurrentLevelName = "Level1";
         }
 
         SceneManager.LoadScene(gameData.CurrentLevelName);
--- Assets/Scripts/Farm/DataSystem/SaveManagerEditor.cs
+++ Assets/Scripts/Farm/DataSystem/SaveManagerEditor.cs
@@ -1,36 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEditor;
+// using UnityEngine;
 
-[CustomEditor(typeof(SaveLoadSystem))]
-public class SaveManagerEditor : UnityEditor.Editor
-{
-    public override void OnInspectorGUI()
-    {
-        SaveLoadSystem saveLoadSystem= (SaveLoadSystem)target;
-        string gameName = saveLoadSystem.gameData.Name;
+// [CustomEditor(typeof(SaveLoadSystem))]
+// public class SaveManagerEditor : Editor
+// {
+//     public override void OnInspectorGUI()
+//     {
+//         SaveLoadSystem saveLoadSystem= (SaveLoadSystem)target;
+//         string gameName = saveLoadSystem.gameData.Name;
 
-        DrawDefaultInspector();
+//         DrawDefaultInspector();
 
-        if (GUILayout.Button("New Game"))
-        {
-            saveLoadSystem.NewGame();
-        }
+//         if (GUILayout.Button("New Game"))
+//         {
+//             saveLoadSystem.NewGame();
+//         }
 
-        if (GUILayout.Button("Save Game"))
-        {
-            saveLoadSystem.SaveGame();
-        }
+//         if (GUILayout.Button("Save Game"))
+//         {
+//             saveLoadSystem.SaveGame();
+//         }
         
-        if (GUILayout.Button("Load Game"))
-        {
-            saveLoadSystem.LoadGame(gameName);
-        }
+//         if (GUILayout.Button("Load Game"))
+//         {
+//             saveLoadSystem.LoadGame(gameName);
+//         }
         
-        if (GUILayout.Button("Delete Game"))
-        {
-            saveLoadSystem.DeleteGame(gameName);
-        }
-    }
-}
+//         if (GUILayout.Button("Delete Game"))
+//         {
+//             saveLoadSystem.DeleteGame(gameName);
+//         }
+//     }
+// }
